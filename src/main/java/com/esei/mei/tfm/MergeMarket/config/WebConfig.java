37791acf605cd15e75ae61cbb9a67871917a4c00
Therimbox <alexandreferi13@gmail.com>
2025-11-03 14:48:26 +0100

    Create WEbconfig.java

---
+++ src/main/java/com/esei/mei/tfm/MergeMarket/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.esei.mei.tfm.MergeMarket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("http://84.246.213.41:8081") // Dominio del frontend en producción
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los headers
+                .allowCredentials(true); // Permitir credenciales (si es necesario)
+    }
+}
