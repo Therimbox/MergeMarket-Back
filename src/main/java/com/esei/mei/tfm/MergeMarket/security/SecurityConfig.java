183a8cf17638e0ab971421bbcc7045b993fed9de
Therimbox <alexandreferi13@gmail.com>
2025-11-03 15:23:08 +0100

    Se corrige la config de seguridad

--- src/main/java/com/esei/mei/tfm/MergeMarket/security/SecurityConfig.java
+++ src/main/java/com/esei/mei/tfm/MergeMarket/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Permitir solicitudes OPTIONS
             .antMatchers("/api/**").permitAll()
             .anyRequest().authenticated()
             .and()
